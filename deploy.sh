7fc6c93d185799099799d5d37bf2570f0321b1cd
wb <1098160709@qq.com>
2021-10-14 00:06:21 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 git push -f git@github.com:newwbbie/newwbbie.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:newwbbie.git master:gh-pages
+#git push -f git@github.com:newwbbie.git master:gh-pages
 
 cd -
